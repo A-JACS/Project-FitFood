d90a5e9a46d6bd30dabad5d815d1129d0e172215
jimmynicks1 <Jamesnickele@gmail.com>
2018-04-05 21:44:14 -0500

    update to git

--- bmi.js
+++ bmi.js
@@ -1,6 +1,6 @@
 
 var app = angular.module('bmiApp', []);
- 
+
 //Text display
 app.controller('headerText', function ($scope){
   $scope.mainHeader="BMI & BMR Calculator";
@@ -27,48 +27,46 @@ var bmr;
         
 function calc() {
   var age = document.getElementById("age").value;
-  var gender = document.getElementById("gender").value;
+
   var heightUS = document.getElementById("heightUS").value;
   var weightUS = document.getElementById("weightUS").value;
-  if (gender == "masc") {
-  bmr = 66.5 + ( 13.75 * weightUS ) + ( 5.003 * heightUS ) - ( 6.755 * age );
+  var activity = $('#activity option:selected').val();
+  var gender = $('input[name=gender]:checked').val();
+
+  if (gender == "male") {
+    bmr = 66.5 + ( 13.75 * weightUS ) + ( 5.003 * heightUS ) - ( 6.755 * age );
   }
   else {
-  bmr = 655.1 + ( 9.563 * weightUS ) + ( 1.850 * heightUS ) - ( 4.676 * age );
+    bmr = 655.1 + ( 9.563 * weightUS ) + ( 1.850 * heightUS ) - ( 4.676 * age );
+  }
+  switch (activity) {
+    case "1":
+     bmr *= 1.2;
+        break;
+    case "2":
+     bmr *= 1.375
+        break;
+    case "3":
+     bmr *= 1.53;
+        break;
+    case "4":
+     bmr *= 1.725;
+        break;
+    case "5":
+     bmr *= 1.9;
+        break;
   }
 }
 
 document.getElementsByTagName("button")[0].addEventListener("click", function() {
   calc();
-  document.getElementById('lblResult').innerHTML = bmr;
+  document.getElementById('caloriesneeded').innerHTML = bmr;
 })
 
-
-
-document.getElementsByTagName("button")[0].addEventListener("click", function() {
-  calc();
-  document.getElementById('lblResult').innerHTML = bmr;
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function toggle_visibility(id) {
-   var e = document.getElementById(id);
-   if(e.style.display == 'block')
-      e.style.display = 'none';
-   else
-      e.style.display = 'block';
-}
+//function toggle_visibility(id) {
+  // var e = document.getElementById(id);
+  // if(e.style.display == 'block')
+   //   e.style.display = 'none';
+ //  else
+ ///     e.style.display = 'block';
+//}
